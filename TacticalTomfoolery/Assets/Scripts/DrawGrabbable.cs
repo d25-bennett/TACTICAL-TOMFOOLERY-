04dc5a41687b44cdd34c89b909260c69ef18db1f
Daniel <daniel9.bennett@live.uwe.ac.uk>
2020-04-02 00:08:59 +0100

    Events manager and draws work (kinda)
    Added a basic event manager that can be called to start new lines of dialog (which will be done soon™) and the draws kinda work. They hold items and nicely keep them inside when closing, but you need to hold, flick and release to either open or close them. Will fix once I know what's the issue.

---
+++ TacticalTomfoolery/Assets/Scripts/DrawGrabbable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGrabbable : OVRGrabbable
+{
+	public Transform handler;
+
+	public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+	{
+		base.GrabEnd(Vector3.zero, Vector3.zero);
+		transform.position = handler.transform.position;
+		transform.rotation = handler.transform.rotation;
+
+		Rigidbody rbHandler = handler.GetComponent<Rigidbody>();
+		rbHandler.velocity = Vector3.zero;
+		rbHandler.angularVelocity = Vector3.zero;
+	}
+
+
+	private void Update()
+	{
+		if (Vector3.Distance(handler.position,transform.position) > 0.4f)
+		{
+			grabbedBy.ForceRelease(this);
+		}
+	}
+
+}
