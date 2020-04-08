58d5a52595a3f230fa2dd13b59258b8c9dab2393
Drew <Drew2.Mayhew@live.uwe.ac.uk>
2020-04-08 20:18:45 +0100

    Altered Hierarchy, Prefabs, Scene

---
+++ TacticalTomfoolery/Assets/Scripts/ImpactEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactEffect : MonoBehaviour
+{
+    public AudioSource impactNoise;
+    void Start()
+    {
+        impactNoise = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        impactNoise.Play();
+    }
+}
